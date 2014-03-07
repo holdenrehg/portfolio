49de989718a72fd28818d86f8423c4cd26ede53a
Holden Rehg <holdenrehg@gmail.com>
2014-03-06 23:54:08 -0600

    setup express directories and hogan

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello Heroku!');
 });
 
 var port = Number(process.env.PORT || 5000);
